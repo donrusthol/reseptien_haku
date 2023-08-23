eb4e8625860669ca274ad7fcddaabf81969c21ee
donrusthol <roope.rustholkarhu@gmail.com>
2023-08-23 13:17:45 +0300

    initialized and created changes

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 
 export default function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=tomato')
+      .then(response => response.json())
+      .then(data => setRecipes(data.meals))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={recipes}
+        keyExtractor={item => item.idMeal}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{item.strMeal}</Text>
+            <Image style={styles.image} source={{ uri: item.strMealThumb }} />
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -16,5 +33,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    padding: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  image: {
+    width: 100,
+    height: 100,
   },
 });
